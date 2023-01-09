dbcef17ec6064efe05d6c735b9b8aaf034cccf41
supermaxio <maxiomeier64@gmail.com>
2023-01-09 00:41:51 -0600

    New URLs

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -1,13 +1,17 @@
 var apiDomain = "";
 switch (process.env.NODE_ENV) {
   case "development":
-    apiDomain = "https://nflplayoffbracket-test-rof7qukjwa-uc.a.run.app";
+    // apiDomain = "https://playoffbracketapi-test-rof7qukjwa-uc.a.run.app";
+    apiDomain = "https://playoffbracketapi-rof7qukjwa-uc.a.run.app";
     break;
   case "test":
-    apiDomain = "https://nflplayoffbracket-test-rof7qukjwa-uc.a.run.app";
+    apiDomain = "https://playoffbracketapi-test-rof7qukjwa-uc.a.run.app";
+    break;
+  case "production":
+    apiDomain = "https://playoffbracketapi-rof7qukjwa-uc.a.run.app";
     break;
   default:
-    apiDomain = "https://nflplayoffbracket-test-rof7qukjwa-uc.a.run.app";
+    apiDomain = "https://playoffbracketapi-test-rof7qukjwa-uc.a.run.app";
     break;
 }
 
