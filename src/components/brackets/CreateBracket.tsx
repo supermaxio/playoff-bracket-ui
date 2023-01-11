33ba3e4117f828101273b1bda4b1bb7276241a13
supermaxio <maxiomeier64@gmail.com>
2023-01-10 19:18:12 -0600

    typo fixes

--- src/components/brackets/CreateBracket.tsx
+++ src/components/brackets/CreateBracket.tsx
@@ -1,36 +1,9 @@
-
-import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Copyright from '../miscellaneous/copyright';
-import { useEffect, useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom';
-import useAuth from '../../hooks/useAuth';
-import useAxiosPrivate from '../../hooks/useAxiosPrivate';
 import ToggleSelection from '../toggles/ToggleSelection';
 
-
 export default function CreateBracket() {
-
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
-
-    const axiosPrivate = useAxiosPrivate();
-    const { setAuth }: any = useAuth();
-    const navigate = useNavigate();
-    // const location = useLocation();
-    // const from = location.state?.from?.pathname || "/"
-
-    useEffect(() => {
-        setErrMsg('');
-    }, []);
-
-    const signout = () => {
-        // signOut();
-        setAuth({});
-        navigate("/login");
-    };
-
 
     return (
         <Container component="main" maxWidth="lg">
