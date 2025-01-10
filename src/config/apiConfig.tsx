20f8463fb2fb8d5e933240edd5cc1b0e7332fe16
supermaxio <maxiomeier64@gmail.com>
2025-01-09 19:18:58 -0600

    2025 playoff init

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -1,14 +1,14 @@
 var apiDomain = "";
 switch (process.env.NODE_ENV) {
   case "development":
-    //apiDomain = "http://localhost:8080";
-    apiDomain = "https://playoff-brackets-api-f6d44ce7cf4f.herokuapp.com";
+    // apiDomain = "http://localhost:8080";
+    apiDomain = "https://playoff-bracket-api.onrender.com";
     break;
   case "test":
-    apiDomain = "https://playoff-brackets-api-f6d44ce7cf4f.herokuapp.com";
+    apiDomain = "https://playoff-bracket-api.onrender.com";
     break;
   case "production":
-    apiDomain = "https://playoff-brackets-api-prod-fdd0929ebae6.herokuapp.com";
+    apiDomain = "https://playoff-bracket-api-prod.onrender.com";
     break;
   default:
     apiDomain = "localhost:8080";
