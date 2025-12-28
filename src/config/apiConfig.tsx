7719163ee8c2faf656ba191af576ec1ecb4c60cb
supermaxio <maxiomeier64@gmail.com>
2025-12-27 20:57:39 -0600

    Point to digital ocean api dev

--- src/config/apiConfig.tsx
+++ src/config/apiConfig.tsx
@@ -2,11 +2,12 @@ var apiDomain = "";
 switch (process.env.NODE_ENV) {
   case "development":
     // apiDomain = "https://playoff-bracket-api-u1qy.onrender.com";
-    apiDomain = "http://localhost:8080";
+    apiDomain = "https://playoff-bracket-api-dev-uhuyr.ondigitalocean.app";
+    // apiDomain = "http://localhost:8080";
     // apiDomain = "https://playoff-bracket-api.onrender.com";
     break;
   case "test":
-    apiDomain = "https://playoff-bracket-api.onrender.com";
+    apiDomain = "https://playoff-bracket-api-dev-uhuyr.ondigitalocean.app";
     break;
   case "production":
     apiDomain = "https://playoff-bracket-api-u1qy.onrender.com";
